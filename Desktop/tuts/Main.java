da5d491053bc04578ef293c831ee1c55688f1791
George Ochieng <geochieng2005@gmail.com>
2026-06-30 19:15:15 +0300

    Add Main class for network calculations

---
+++ Desktop/tuts/Main.java
@@ -0,0 +1,39 @@
+package tuts;
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main (String [] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.println("\n1. Network Requirement.\n2. Host Requirements.");
+        int mode;
+
+        do {
+        System.out.println("Choose the calculation Mode(1 or 2): ");
+        mode = cin.nextInt();
+        cin.nextLine();
+
+        if (mode != 1 && mode != 2) {
+            System.out.print("Invalid Choice, Try again!");
+        }
+        } while (mode != 1 && mode != 2);
+
+        if (mode == 1) {
+            System.out.println("Enter the number of hosts required : ");
+            int hostsreq = cin.nextInt();
+            cin.nextLine();
+
+            NetCalculator calc = new NetCalculator();
+
+            System.out.println();
+            System.out.println("Analysis");
+            System.out.println((256 / calc.getIncrement(calc.hostbits(hostsreq))) +" Subnets on the Network.");
+            System.out.println("Your Network Would have "+calc.getIncrement(calc.hostbits(hostsreq)) + " per subnet.");
+            System.out.println("CIDR: /" +calc.cidr(calc.hostbits(hostsreq)));
+            System.out.println("Capacity in each Subnet : " + calc.getIncrement(calc.hostbits(hostsreq)));
+        }
+       
+        cin.close();
+    }
+};
