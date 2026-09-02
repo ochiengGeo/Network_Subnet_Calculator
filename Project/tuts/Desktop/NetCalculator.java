64a12f1f92da230a89312d9ca2ef5d7e9a3e7a65
George Ochieng <geochieng2005@gmail.com>
2026-09-03 01:22:44 +0300

    Swapped the directories

---
+++ Project/tuts/Desktop/NetCalculator.java
@@ -0,0 +1,19 @@
+package tuts.Desktop;
+public class NetCalculator {
+    public static void main(String[] args) {
+        
+    }
+       public int hostbits (int hostsreq) {
+        
+        return (int)(Math.log(hostsreq + 2)/Math.log(2));
+    }
+
+   public int getIncrement (int hostbits) {
+        return (int)(Math.pow(2, hostbits));
+     }
+    public int cidr (int hostbits) {
+        return 32 - hostbits;
+    }
+}
+ 
+
